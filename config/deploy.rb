ce9d55f8b6ea2493bf97c74f18315c8a32a0ce0f
Yann Lugrin <yann.lugrin@liquid-concept.ch>
2013-03-11 18:26:27 +0100

    change deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,11 +1,11 @@
 # encoding: utf-8
 set :application, 'website'
 set :domain,      'epsn.ch'
-set :server_name, 'ns3.liquid-concept.ch'
+set :server_name, 'ruby-rack-2.liquid-concept.ch'
 
 set :scm,         :git
-set :repository,  'git@git.liquid-concept.ch:clients/epsn/website.git'
-set :branch,      'master'
+set :repository,  '.'
+set :branch,      'production'
 
 ssh_options[:forward_agent] = true
 
@@ -13,7 +13,7 @@ default_run_options[:pty] = true
 default_environment['LC_CTYPE'] = 'en_US.UTF-8'
 
 set :user,        'webpublisher'
-set :deploy_via,  :remote_cache
+set :deploy_via,  :copy
 set :deploy_to,   "/var/www/#{domain}/#{application}"
 set :use_sudo,    false
 
@@ -21,18 +21,22 @@ role :web, server_name                          # Your HTTP server, Apache/etc
 role :app, server_name                          # This may be the same as your `Web` server
 role :db,  server_name, :primary => true        # This is where Rails migrations will run
 
+set :copy_exclude, ".git/*"
+set :build_script, "bundle install && RACK_ENV=\"production\" bundle exec nanoc compile"
+
 namespace :deploy do
   task :start, :roles => :app do
+    run "mkdir -p #{File.join(current_path,'tmp')}"
     run "touch #{File.join(current_path,'tmp','restart.txt')}"
   end
   task :stop do ; end
   task :restart, :roles => :app, :except => { :no_release => true } do
+    run "mkdir -p #{File.join(current_path,'tmp')}"
     run "touch #{File.join(current_path,'tmp','restart.txt')}"
   end
 end
 
 before 'deploy:symlink' do
-  run "cd #{release_path} && RACK_ENV=\"production\" bundle exec nanoc3 compile > /dev/null"
 end
 
 after 'deploy:update', 'deploy:restart'
